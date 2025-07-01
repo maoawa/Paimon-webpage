2f16f8f50e01a3aa5f1a7fae8b7c7ef506f41ce6
Kuriyota <Kuriyota@outlook.com>
2025-07-01 22:22:08 +0800

    Using Vite

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Paimon-webpage
+
 一个关于原神派蒙的简单网页/A simple web page about Paimon(Genshin)
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
+});
